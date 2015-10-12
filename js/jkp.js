351815063601ad55f556f900596362c2a2f6e94a
Cristian Morataya <cris.morataya@gmail.com>
2015-10-11 19:52:42 -0600

    Pregunta al usuario la opcion a elegir

--- js/jkp.js
+++ js/jkp.js
@@ -1,1 +1,23 @@
-alert("works?");
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+
+var opcionUsuario;
+var opcionMaquina;
+
+opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+//alert ("Elegiste " + opcionUsuario);
+
+if (opcionUsuario == 0)
+{
+    alert("Elegiste piedra");
+}
+else if (opcionUsuario == 1)
+{
+    alert("Elegiste papel");
+}
+else if (opcionUsuario == 2)
+{
+    alert("Elegiste tijera");
+}
