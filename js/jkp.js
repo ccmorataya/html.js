56503894d2928e4fde8b75b7c2d80a556c722eee
Cristian Morataya <cris.morataya@gmail.com>
2015-10-11 20:15:25 -0600

    Adición de ociones al condicional

--- js/jkp.js
+++ js/jkp.js
@@ -12,14 +12,53 @@ opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
 if (opcionUsuario == piedra)
 {
     alert("Elegiste piedra");
+
+    if (opcionMaquina == piedra)
+    {
+        alert("Empate");
+    }
+    else if (opcionMaquina == papel)
+    {
+        alert("Perdiste");
+    }
+    else if (opcionMaquina == tijera)
+    {
+        alert("Ganaste");
+    }
 }
 else if (opcionUsuario == papel)
 {
     alert("Elegiste papel");
+
+    if (opcionMaquina == piedra)
+    {
+        alert("Ganaste");
+    }
+    else if (opcionMaquina == papel)
+    {
+        alert("Empate");
+    }
+    else if (opcionMaquina == tijera)
+    {
+        alert("Perdiste");
+    }
 }
 else if (opcionUsuario == tijera)
 {
     alert("Elegiste tijera");
+
+    if (opcionMaquina == piedra)
+    {
+        alert("Perdiste");
+    }
+    else if (opcionMaquina == papel)
+    {
+        alert("Ganaste");
+    }
+    else if (opcionMaquina == tijera)
+    {
+        alert("Empate");
+    }
 }
 else
 {
