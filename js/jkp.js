1db43b9094972ec90b02da8e9a7b18499f2ba3c8
Cristian Morataya <cris.morataya@gmail.com>
2015-10-11 20:06:10 -0600

    agregada la opción de la maquina

--- js/jkp.js
+++ js/jkp.js
@@ -3,7 +3,7 @@ var papel = 1;
 var tijera = 2;
 
 var opcionUsuario;
-var opcionMaquina;
+var opcionMaquina = papel;
 
 opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
 
@@ -20,4 +20,8 @@ else if (opcionUsuario == papel)
 else if (opcionUsuario == tijera)
 {
     alert("Elegiste tijera");
+}
+else
+{
+    alert("No elegiste opción");
 }
