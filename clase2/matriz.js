98e838eabaf434d3b458f49db3c17299fc3bd562
Cristian Morataya <cris.morataya@gmail.com>
2016-06-21 13:47:53 -0600

    ejemplos de matrices

---
+++ clase2/matriz.js
@@ -0,0 +1,5 @@
+//var menu = ["productos", "ventas", "contacto"];
+//document.write(menu[0]);
+
+var foda = [ ["fortaleza", "oportunidad"], ["debilidades", "amenazas"] ];
+document.write(foda[1][1]);
